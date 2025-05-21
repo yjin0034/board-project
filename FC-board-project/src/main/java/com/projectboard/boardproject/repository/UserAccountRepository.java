be3799fa3f23fbabca77f337eac6e1a5fa8c00d7
yjin0034 <yjin0034@naver.com>
2025-05-22 04:27:50 +0900

    #17 - 회원 계정 도메인 구현 및 erd 반영
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

---
+++ FC-board-project/src/main/java/com/projectboard/boardproject/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.projectboard.boardproject.repository;
+
+import com.projectboard.boardproject.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
